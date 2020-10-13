0aa5862ac292a8edf4f8369c7aa04268e5848183
justshashi8722 <justshashi872@gmail.com>
2020-10-13 11:51:38 +0530

    scommit

---
+++ fibonicc14.js
@@ -0,0 +1,18 @@
+var i=0;
+var j=1;
+var result;
+var num= 10;
+console.log(i)
+console.log(j)
+
+for(var k=3;k<=num;k++){
+
+	
+	result=i+j;
+	i=j;
+	j=result;
+	console.log(result)
+
+	//**document.write(result+"<br/>");
+//-->
+}
