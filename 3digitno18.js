44a9d1568134ce9241b89a556808767d40b2ce0a
justshashi8722 <justshashi872@gmail.com>
2020-10-13 12:00:04 +0530

    commit3

---
+++ 3digitno18.js
@@ -0,0 +1,10 @@
+
+	var i= 875;
+	i=i.toString().length;
+	if(i==3)
+	{
+	console.log('given number is 3digitnumber')
+	}
+	else{
+	console.log('given number is not 3digitnumber')
+	}
---
+++ 4units17.js
@@ -0,0 +1,9 @@
+
+	var i= 47;
+	if(i%10 ==4)
+	{
+	console.log('given number has a 4 at units')
+	}
+	else{
+	console.log('there is no 4 at units in given number')
+	}
